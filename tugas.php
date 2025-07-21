87adfe65897ad571cd5a720e9f133974f07839c1
adtyfp <iijo8362@gmail.com>
2025-07-21 16:00:00 +0700

    Update tugas.php

--- tugas.php
+++ tugas.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 // koneksi
-$koneksi = new mysqli("localhost", "root", "", "apkkelasvirtual");
+$koneksi = new mysqli("shortline.proxy.rlwy.net", "root", "", "railway");
 
 $tugas = [];
 $kuis = [];
