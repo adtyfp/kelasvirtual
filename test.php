0e90b04fe4b01da1e8790c787579cee147952cdf
adtyfp <iijo8362@gmail.com>
2025-07-22 10:32:36 +0700

    Update test.php

--- test.php
+++ test.php
@@ -3,13 +3,14 @@ require_once 'koneksi.php';
 
 try {
     // Gunakan prepared statement agar aman dari SQL Injection
-    $stmt = $pdo->prepare("INSERT INTO users (name, email, nim, password) VALUES (:name, :email, :nim, :password)");
+    $stmt = $pdo->prepare("INSERT INTO users (name, email, nim, note, password) VALUES (:name, :email, :nim, :note, :password)");
 
     // Contoh data dummy
     $stmt->execute([
         'name'     => 'Tes User',
         'email'    => 'tes@email.com',
         'nim'    => '11223344',
+        'note'    => '11223344',
         'password' => password_hash('12345', PASSWORD_BCRYPT)
     ]);
 
