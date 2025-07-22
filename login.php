6ee78817a58d2139bd09290c3447f0237713e5c4
adtyfp <iijo8362@gmail.com>
2025-07-22 10:40:31 +0700

    Update login.php

--- login.php
+++ login.php
@@ -19,14 +19,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $email_nim = trim($_POST['email_nim']);
         $password = $_POST['password'];
 
+        // Cek apakah input merupakan email atau NIM
         $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ? OR nim = ?");
         $stmt->execute([$email_nim, $email_nim]);
         $user = $stmt->fetch();
 
         if ($user && password_verify($password, $user['password'])) {
+            // Login sukses
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['user_name'] = $user['name'];
 
+            // Buat cookie 1 hari
             setcookie('user_id', $user['id'], time() + 86400, "/");
             setcookie('user_name', $user['name'], time() + 86400, "/");
 
@@ -37,6 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $active_tab = 'login';
         }
     }
+}
 
     // === REGISTER ===
     if (isset($_POST['action']) && $_POST['action'] === 'register') {
